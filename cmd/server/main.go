77c681201e021863ce61ad935e46f455ff20476a
Эльдар Гусейнов <you@example.com>
2022-05-24 10:44:56 +0300

    Initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func appruve(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Header)
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/update/", appruve)
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/h34dl355/metric-service
+
+go 1.17
